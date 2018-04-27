3a0cbe8134cb5f859ca1f419b79cb4d8936da756
xiaopeng <857763476@qq.com>
2018-04-27 17:11:34 +0800

    修改项目上线的地址

--- src/api/recommend.js
+++ src/api/recommend.js
@@ -2,6 +2,8 @@ import jsonp from 'common/js/jsonp'
 import axiox from 'axios'
 import {commonParams, options} from './config'
 
+const debug = process.env.NODE_ENV !== 'production'
+
 export function getRecommend() {
   const url = 'https://c.y.qq.com/musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg'
   const data = Object.assign({}, commonParams, {
@@ -13,7 +15,8 @@ export function getRecommend() {
 }
 
 export function getDiscList() {
-  const url = '/api/getDiscList'
+  const url = debug ? '/api/getDiscList' : 'http://www.jiajiamusic.com/music/api/getDiscList'
+  // const url = '/api/getDiscList'
   const data = Object.assign({}, commonParams, {
     hostUin: 0,
     needNewCode: 1,
@@ -32,7 +35,8 @@ export function getDiscList() {
 }
 
 export function getSongList(disstid) {
-  const url = '/api/getSongList'
+  const url = debug ? '/api/getSongList' : 'http://www.jiajiamusic.com/music/api/getSongList'
+  // const url = '/api/getSongList'
   const data = Object.assign({}, commonParams, {
     type: 1,
     json: 1,
